91eb9153ccfd86b957093d2b5af45cc6034d424b
Henry Ji <hj465@cornell.edu>
2025-10-15 11:55:08 -0400

    init

---
+++ src/cs2110/ListUtilities.java
@@ -0,0 +1,27 @@
+package cs2110;
+
+import java.util.Iterator;
+
+/**
+ * Contains static methods for checking whether a list is mirrored.
+ */
+public class ListUtilities {
+
+    /**
+     * Returns whether this list is "mirrored" meaning it produces the same sequence of elements
+     * when iterated in the forward or reverse directions.
+     */
+    public static <T> boolean isMirrored1(CS2110List<T> list) {
+        // TODO 5: Implement this method according to its specifications.
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns whether this list is "mirrored" meaning it produces the same sequence of elements
+     * when iterated in the forward or reverse directions.
+     */
+    public static <T> boolean isMirrored2(DoublyLinkedList<T> list) {
+        // TODO 7: Implement this method according to its specifications.
+        throw new UnsupportedOperationException();
+    }
+}
