a0312fecd86c11a6fcf67c2079c927429a597ba6
Jonathan <jonathan.rh.lin@gmail.com>
2025-10-14 21:59:39 -0400

    completed 2.1 - 2.2c

--- src/cs2110/DoublyLinkedList.java
+++ src/cs2110/DoublyLinkedList.java
@@ -3,8 +3,8 @@ package cs2110;
 import java.util.Iterator;
 
 /**
- * A list of elements of type `T` implemented using a doubly-linked chain.
- * `null` may not be stored as an element in this list.
+ * A list of elements of type `T` implemented using a doubly-linked chain. `null` may not be stored
+ * as an element in this list.
  */
 public class DoublyLinkedList<T> implements CS2110List<T> {
 
@@ -12,13 +12,20 @@ public class DoublyLinkedList<T> implements CS2110List<T> {
      * A node of a doubly-linked list whose elements have type `T`.
      */
     private class DNode {
-        /** The element in this node. */
+
+        /**
+         * The element in this node.
+         */
         T data;
 
-        /** The next node in the chain (or null if this is the last node). */
+        /**
+         * The next node in the chain (or null if this is the last node).
+         */
         DNode next;
 
-        /** The previous node in the chain (or null if this is the first node). */
+        /**
+         * The previous node in the chain (or null if this is the first node).
+         */
         DNode prev;
 
         /**
@@ -61,11 +68,25 @@ public class DoublyLinkedList<T> implements CS2110List<T> {
             assert head.prev == null;
             assert tail != null;
 
-            // TODO 1: By traversing the list from head to tail, check that
-            //  (1) none of the nodes store null elements
-            //  (2) the number of linked nodes is equal to the list's size
-            //  (3) the last linked node is the same object as `tail`
-            //  (4) the linking is consistent, i.e., for a non-tail node n, n.next.prev is n
+            int lengthSoFar = 0;
+            DNode current = head;
+            DNode prevNode = null;
+            while (current != null) {
+                assert current.data != null;
+                assert current.prev == prevNode;
+                if (current != tail) {
+                    assert current.next.prev == current;
+                }
+                if (current != head) {
+                    assert current.prev.next == current;
+                }
+                prevNode = current;
+                current = current.next;
+                lengthSoFar++;
+            }
+            assert prevNode == tail;
+            assert lengthSoFar == size();
+
         }
     }
 
@@ -79,14 +100,57 @@ public class DoublyLinkedList<T> implements CS2110List<T> {
         assertInv();
     }
 
+    /**
+     * Returns the DNode at a given index position Preconditions: index >= 0 && index < size
+     *
+     */
+    private DNode findDNode(int index) {
+        assert index >= 0 && index < size();
+        DNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
     @Override
     public void add(T elem) {
-        // TODO 2a: Implement this method according to its specifications.
+        assert elem != null;
+        insert(size, elem);
+        assertInv();
     }
 
     @Override
     public void insert(int index, T elem) {
-        // TODO 2b: Implement this method according to its specification
+        assert elem != null;
+        assert index >= 0 && index <= size();
+
+        if (index == 0){ //adding to beginning
+            DNode newNode = new DNode(elem, null, head);
+            if (head != null){ //list is not empty
+                head.prev = newNode;
+            }
+            else{ //need to update tail if the list was empty
+                tail = newNode;
+            }
+            head = newNode;
+        }
+        else if(index == size){ //adding to end
+            DNode newNode = new DNode(elem, tail, null);
+            tail.next = newNode;
+            tail = newNode;
+        }
+        else {//regular case, not adding to front or end
+            //current = first DNode that will be shifted to the right
+            DNode current = findDNode(index);
+
+            DNode newNode = new DNode(elem,current.prev, current);
+            current.prev.next = newNode;
+            current.prev = newNode;
+
+        }
+        size++;
+        assertInv();
     }
 
     @Override
@@ -96,8 +160,9 @@ public class DoublyLinkedList<T> implements CS2110List<T> {
 
     @Override
     public T get(int index) {
-        // TODO 2c: Implement this method according to its specifications.
-        throw new UnsupportedOperationException();
+        assert index >= 0 && index < size();
+        return findDNode(index).data;
+
     }
 
     @Override
@@ -130,7 +195,8 @@ public class DoublyLinkedList<T> implements CS2110List<T> {
 
     /**
      * Return an iterator over the elements of this list (in forward order). To ensure the
-     * functionality of this iterator, this list should not be mutated while the iterator is in use.
+     * functionality of this iterator, this list should not be mutated while the iterator is in
+     * use.
      */
     @Override
     public Iterator<T> iterator() {
@@ -144,13 +210,16 @@ public class DoublyLinkedList<T> implements CS2110List<T> {
      * lifetime of this iterator.
      */
     private class ForwardIterator implements Iterator<T> {
+
         /**
          * The node whose value will next be returned by the iterator, or null once the iterator
          * reaches the end of the list.
          */
         private DNode nextToVisit;
 
-        /** Constructs a new ForwardIterator over this list */
+        /**
+         * Constructs a new ForwardIterator over this list
+         */
         public ForwardIterator() {
             nextToVisit = head;
         }
